e4619d4b8e1a3152d2f632458fb9442af762a788
Rumpkin <orionvillager@gmail.com>
2024-01-18 23:27:02 -0800

    Task 5
    Implemented the megapiece and passed all the tests

--- src/Piece.java
+++ src/Piece.java
@@ -12,8 +12,26 @@ public class Piece {
 
     private int findRow(int column, Board board) { // given the state of the board, and a column, it returns the row
         int[][] boardPieces = board.getBoardPieces();
+
         if (this instanceof MegaPiece) {
-            return 0;
+            if (column + 1 >= boardPieces.length || column - 1 < 0)
+                return -1;
+
+            if (boardPieces[column][0] != 0 || boardPieces[column - 1][0] != 0 || boardPieces[column + 1][0] != 0)
+                return -1;
+
+            for (int i = 0; i < boardPieces[column].length; i++) {
+                if (boardPieces[column][i] != 0)
+                    return i - 2;
+
+                if (boardPieces[column + 1][i] != 0)
+                    return i - 2;
+
+                if (boardPieces[column - 1][i] != 0)
+                    return i - 2;
+            }
+
+            return boardPieces[column].length - 2;
         } else {
             if (boardPieces[column][0] != 0)
                 return -1;
